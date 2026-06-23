aaeb2979ecb8c986ca51efda3400d58a99040a17
Isadora-Danelon <isadora.danelon@aluno.senai.br>
2026-06-23 10:02:35 -0300

    Isadora - css

--- Front-End/src/pages/Home.jsx
+++ Front-End/src/pages/Home.jsx
@@ -6,13 +6,16 @@ export default function Home() {
     <div>
       <Navbar />
       <div className="container">
-
+      <div className="imagem">
         <div className="home-main">
           <h1 className="t1">Supplix</h1>
           <h1 className="t2">Nexora</h1>
           <p className='slogan'>Empresas que fornecem. Pessoas que recebem</p>
             <Link to="/produtos"><button className="btn btn-primary">Produtos</button></Link>
         </div>
+
+      </div>
+
 
       </div>
     </div>
